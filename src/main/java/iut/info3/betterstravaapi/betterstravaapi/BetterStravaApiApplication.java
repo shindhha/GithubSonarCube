174fc3eb1db2583f1cf8c91fd30429546e9610b4
ClemFR <clementlaurie27@gmail.com>
2023-12-05 16:29:29 +0100

    Initialisation du projet Spring

---
+++ src/main/java/iut/info3/betterstravaapi/betterstravaapi/BetterStravaApiApplication.java
@@ -0,0 +1,13 @@
+package iut.info3.betterstravaapi.betterstravaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterStravaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterStravaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iut/info3/betterstravaapi/betterstravaapi/BetterStravaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package iut.info3.betterstravaapi.betterstravaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterStravaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
