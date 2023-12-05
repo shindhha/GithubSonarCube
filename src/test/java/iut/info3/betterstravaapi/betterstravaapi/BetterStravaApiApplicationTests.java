135b0028cc871fdacd12790d19b14c9afff2de3a
guillaume.medard <guillaume.medard1@gmail.com>
2023-12-05 17:55:02 +0100

    Ajout de plugin maven : cucumber, jacoco, checkstyle, ajout de la configuration par défaut de checkstyle 'checkstyle.xlm'

--- src/test/java/iut/info3/betterstravaapi/betterstravaapi/BetterStravaApiApplicationTests.java
+++ src/test/java/iut/info3/betterstravaapi/betterstravaapi/BetterStravaApiApplicationTests.java
@@ -1,11 +1,11 @@
 package iut.info3.betterstravaapi.betterstravaapi;
 
+import io.cucumber.java.en.Given;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BetterStravaApiApplicationTests {
-
     @Test
     void contextLoads() {
     }
